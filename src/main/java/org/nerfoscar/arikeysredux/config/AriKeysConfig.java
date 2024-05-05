4c195bc420ea8d8083a89f9d685e2548752e65d1
NerfOscar <93657774+NerfOscar@users.noreply.github.com>
2024-05-05 11:25:44 -0400

    Updated to 1.20.4, I fcked up with netty but... It doesn't matter.

---
+++ src/main/java/org/nerfoscar/arikeysredux/config/AriKeysConfig.java
@@ -0,0 +1,54 @@
+package org.nerfoscar.arikeysredux.config;
+
+import org.nerfoscar.arikeysredux.AriKeysReduxMain;
+import lombok.Getter;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class AriKeysConfig {
+	private final Map<NamespacedKey, AriKeyInfo> keyInfoList = new HashMap<>();
+
+	private boolean eventOnCommand;
+
+	// Loads all the keys specified in config.yml and puts them in keyInfoList.
+	public void reload(FileConfiguration config) {
+		eventOnCommand = config.getBoolean("run_event_on_command");
+		keyInfoList.clear();
+		ConfigurationSection keys = config.getConfigurationSection("Keys");
+		if (keys == null) return;
+		for (String key : keys.getKeys(false)) {
+			if (keys.contains(key, true)) {
+				ConfigurationSection section = keys.getConfigurationSection(key);
+				if (section == null) {
+					error(key);
+					return;
+				}
+
+				AriKeyInfo info = AriKeyInfo.from(section);
+				if (info == null) {
+					error(key);
+					return;
+				}
+
+				keyInfoList.put(info.getId(), info);
+			}
+		}
+	}
+
+	private void error(String name) {
+		AriKeysReduxMain.get().getLogger().severe("Unable to add MythicKey: '" + name + "' - Check your syntax!");
+	}
+
+	public Map<NamespacedKey, AriKeyInfo> getKeyInfoList() {
+		return keyInfoList;
+	}
+
+	public boolean isEventOnCommand() {
+		return eventOnCommand;
+	}
+}
